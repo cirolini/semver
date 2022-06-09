11fef3ec98805bf1cd31a9f94d3d6472a834b10f
RC <rc@MacBook-Air-de-RC.local>
2022-06-09 10:12:14 -0300

    feat: first joke done

--- semver_demo/__init__.py
+++ semver_demo/__init__.py
@@ -1,4 +1,4 @@
-"""The funniest joke in the world."""
+"""The funniest joke in the whole world."""
 def joke():
     """The first Joke"""
     return ('Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
